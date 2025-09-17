201520af1b1384699b07283178adcbe8251602e2
Guyon Ganchi <gganchi@iu.edu>
2025-09-17 02:30:14 +0000

    pasting week 2 code in this new codespace since the previous fork had technical issues. no changes were made from the previous submission for integrity purposes.

--- apputil.py
+++ apputil.py
@@ -3,11 +3,35 @@ import numpy as np
 
 # update/add code below ...
 
+# Exercise 1
 def ways(n):
-    return None
+    count = 0
+    for nickels in range(n // 5 + 1):
+        pennies = n - 5 * nickels
+        if pennies >= 0:
+            count += 1
+    return count
 
+
+print(ways(12))
+
+# Exercise 2
+names = np.array(['Hannah', 'Astrid', 'Abdul', 'Mauve', 'Jung'])
+scores = np.array([99, 71, 85, 62, 91])
+
+
+# Part 1
 def lowest_score(names, scores):
-    return None
+    student_lowest = np.argmin(scores)
+    return names[student_lowest]
 
+
+print(lowest_score(names, scores))
+
+# Part 2
 def sort_names(names, scores):
-    return None
+    sort_descend = np.argsort(scores)[::-1]
+    return names[sort_descend]
+
+
+print(sort_names(names, scores))
